d83f5c176cef31e8f1235368ce57b526fbdf8874
divya <gupta.divya1501@gmail.com>
2017-06-19 17:37:42 +0530

    Services : creating service and playing song

---
+++ services/src/main/java/com/example/services/MyIntentService.java
@@ -0,0 +1,28 @@
+package com.example.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+
+//use to work in worker thread
+public class MyIntentService extends IntentService {
+
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+
+
+}
