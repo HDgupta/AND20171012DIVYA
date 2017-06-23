1f6d20c83208e173e982370125229e18f74846dc
divya <gupta.divya1501@gmail.com>
2017-06-23 14:57:38 +0530

    Volley : get data from server

--- http/src/main/java/com/example/http/MainActivity.java
+++ http/src/main/java/com/example/http/MainActivity.java
@@ -10,15 +10,13 @@ import android.widget.TextView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.StringRequest;
-
-import com.example.http.domain.List;
-import com.example.http.domain.Parse;
+import com.example.http.domain.Wea;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = MainActivity.class.getName();
+    public static final String TAG ="@codekul";
+            //MainActivity.class.getName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,17 +31,16 @@ public class MainActivity extends AppCompatActivity {
                 String lon = ((EditText)findViewById(R.id.edtLong)).getText().toString();
 
                 final  ProgressDialog pd = ProgressDialog.show(MainActivity.this,"Inprogress","Loading data..");
-                app.queue().add(new StringRequest("http://api.openweathermap.org/data/2.5/find?lat="+lat+"&lon="+lon+"&cnt=10&appid=eb5b07133b19a226621ddb469133f6aa", new Response.Listener<String>() {
+               // app.queue().add(new StringRequest("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&&lon=" + lon + "&mode=json&units=metric&cnt=7&appid=7406c21bb1cb9f59d936a59c4e890279", new Response.Listener<String>() {
+                app.queue().add(new StringRequest("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&mode=json&units=metric&cnt=7&appid=eb5b07133b19a226621ddb469133f6aa", new Response.Listener<String>() {
+
                     @Override
                     public void onResponse(String response) {
-                        Log.i(TAG,"Response is -" +
-                                response);
+                        Log.i(TAG,"Response is -" + response);
 
-                        Parse m = ((App) getApplication()).gson().fromJson(response,Parse.class);
-                        // String show = m.getMessage();
+                        Wea m = ((App) getApplication()).gson().fromJson(response,Wea.class);
 
-                        List lst = m.getList().get(0);
-                        String show = ""+lst.getMain().getTemp();
+                        String show = m.getSys().getCountry() +","+ m.getMain().getTemp();
                         ((TextView)findViewById(R.id.txtShow)).setText(show);
                         pd.dismiss();
                     }
@@ -60,35 +57,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-//    public void OnFind(View view){
-//        App app = (App) getApplication();
-//
-//        String lat = ((EditText)findViewById(R.id.edLat)).getText().toString();
-//        String lon = ((EditText)findViewById(R.id.edtLong)).getText().toString();
-//
-//        final  ProgressDialog pd = ProgressDialog.show(MainActivity.this,"Inprogress","Loading data..");
-//        app.queue().add(new StringRequest("http://api.openweathermap.org/data/2.5/find?lat="+lat+"&lon="+lon+"&cnt=10&appid=eb5b07133b19a226621ddb469133f6aa", new Response.Listener<String>() {
-//            @Override
-//            public void onResponse(String response) {
-//                Log.i(TAG,"Response is -" +
-//                        response);
-//
-//                Parse m = ((App) getApplication()).gson().fromJson(response,Parse.class);
-//               // String show = m.getMessage();
-//
-//                List lst = m.getList().get(0);
-//               String show = lst.getMain().getTemp()+"";
-//                ((TextView)findViewById(R.id.txtShow)).setText(show);
-//                pd.dismiss();
-//            }
-//        }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                //VolleyLog.wtf(error,"");
-//                pd.dismiss();
-//            }
-//        }));
-//    }
 }
 
 
--- http/src/main/java/com/example/http/domain/Sys.java
+++ http/src/main/java/com/example/http/domain/Sys.java
@@ -6,9 +6,26 @@ import com.google.gson.annotations.SerializedName;
 
 public class Sys {
 
+    @SerializedName("message")
+    @Expose
+    private Double message;
     @SerializedName("country")
     @Expose
     private String country;
+    @SerializedName("sunrise")
+    @Expose
+    private Integer sunrise;
+    @SerializedName("sunset")
+    @Expose
+    private Integer sunset;
+
+    public Double getMessage() {
+        return message;
+    }
+
+    public void setMessage(Double message) {
+        this.message = message;
+    }
 
     public String getCountry() {
         return country;
@@ -16,6 +33,22 @@ public class Sys {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public Integer getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(Integer sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public Integer getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(Integer sunset) {
+        this.sunset = sunset;
     }
 
 }
